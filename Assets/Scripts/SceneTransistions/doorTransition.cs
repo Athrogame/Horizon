5e049bc33770ba188b448dc9ecfd2e67757313ee
Athrogame <aydinakmal2011@gmail.com>
2026-06-06 11:51:49 -0400

    quality of life features

--- Assets/Scripts/SceneTransistions/doorTransition.cs
+++ Assets/Scripts/SceneTransistions/doorTransition.cs
@@ -1,16 +1,86 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class doorTransition : MonoBehaviour
 {
-    public int sceneToLoad; // The build index of the scene to load when the player enters the trigger
+    [Tooltip("Assign Player/Interact from InputSystem_Actions. Falls back to Space/Z/Enter if empty.")]
+    public InputActionReference advanceAction;
+
+    private InputAction advanceInput;
+    private bool playerInside = false;
+    private bool isLoading = false;
+    private bool ownsFallbackAction = false;
+
+    public int sceneToLoad; 
     public int doorToLoad;
+
+    void Awake()
+    {
+        if (advanceAction != null)
+        {
+            advanceInput = advanceAction.action;
+        }
+        else
+        {
+            advanceInput = new InputAction(type: InputActionType.Button);
+            ownsFallbackAction = true;
+            advanceInput.AddBinding("<Keyboard>/space");
+            advanceInput.AddBinding("<Keyboard>/z");
+            advanceInput.AddBinding("<Keyboard>/enter");
+        }
+    }
+
+    void OnEnable()
+    {
+        if (advanceInput == null)
+            return;
+
+        advanceInput.started += OnInteract;
+        advanceInput.Enable();
+    }
+
+    void OnDisable()
+    {
+        if (advanceInput != null)
+            advanceInput.started -= OnInteract;
+
+        if (ownsFallbackAction)
+            advanceInput.Disable();
+    }
+
+    void OnDestroy()
+    {
+        if (ownsFallbackAction)
+            advanceInput.Dispose();
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
+            playerInside = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            playerInside = false;
+    }
+
+    private void OnInteract(InputAction.CallbackContext ctx)
+    {
+        if (!playerInside || isLoading)
+            return;
+
+        isLoading = true;
+
+        if (SceneMgr.I != null)
         {
             SceneMgr.I.doorToSpawnAt = doorToLoad;
             SceneMgr.I.LoadScene(sceneToLoad);
+            return;
         }
+
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
